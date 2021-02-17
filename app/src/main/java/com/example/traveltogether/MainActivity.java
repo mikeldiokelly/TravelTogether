91b10c283121a7595a1ceaa8bb72fafb9dc6059f
Prabhjot Sandhu <sandhu7707@gmail.com>
2021-02-17 22:23:03 +0000

    current location on maps and p2p service register/discovery

--- app/src/main/java/com/example/traveltogether/MainActivity.java
+++ app/src/main/java/com/example/traveltogether/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.traveltogether;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button login_register, login, forgot;
+    private Button login_register, login, forgot, p2p, map;
     private EditText login_email, login_pass;
 
     private FirebaseAuth mauth;
@@ -31,6 +32,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         login_register = (Button) findViewById(R.id.login_register);
         login_register.setOnClickListener(this);
@@ -44,6 +51,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         login_email = (EditText) findViewById(R.id.login_email);
         login_pass = (EditText) findViewById(R.id.login_password);
         loginBar = (ProgressBar) findViewById(R.id.login_progressBar);
+
+        p2p = (Button) findViewById(R.id.p2pButton);
+        p2p.setOnClickListener(this);
+
+        map = (Button) findViewById(R.id.map);
+        map.setOnClickListener(this);
 
         mauth = FirebaseAuth.getInstance();
         if (mauth.getCurrentUser() != null) {
@@ -63,6 +76,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.forgot:
                 startActivity(new Intent(this, ForgotPasswordActivity.class));
                 break;
+            case R.id.p2pButton:
+                startActivity(new Intent(this, PeerToPeer.class));
+            case R.id.map:
+                startActivity(new Intent(this, MapsActivity.class));
         }
     }
 
