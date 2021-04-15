d67cf35ac67ce71c0f4f6477a0ce286fa58192a3
caop <820672172@qq.com>
2021-04-15 17:53:49 +0100

    cloud functions

---
+++ cloud_functions/functions/index.js
@@ -0,0 +1,16 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+// called when new data is written
+exports.newNodeDetected = functions.database.ref("/Journeys/")
+    .onCreate( (snapshot, context) => {
+      const newj = snapshot.val();
+      console.log(newj);
+    });
