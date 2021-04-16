a5835d1c7be91fbc00ae982f6d258b58b651200f
caop <820672172@qq.com>
2021-04-16 18:09:32 +0100

    modify cloud functions

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,5 +14,20 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.newNodeDetected = functions.database.ref("/Journeys/")
     .onCreate( (snapshot, context) => {
       const newj = snapshot.val();
-      console.log(newj);
+      // Notification details.
+      const payload = {
+        notification: {
+          title: 'The Journey List Updates!!!',
+          body: `${newj.firstUser} creates a new journey.`,
+        }
+      };
+      let token;
+      token = "cASj3iaXSwKBLxhkNdzHMc:APA91bFj7RZ2lHv2iQDpPoSF9ZWkDaN3PX9ElB955ywcl_GHt5nQShCAcUfb_yqlaNSYnkHaFz97w66LGh3-JJNnmSsmdfEyqqHKTu9ZvuLbjrta-2YlUH4qiXd33jG0r07PzrTdEJyN"
+      // send notifications to all tokens
+      const response = await admin.messaging().sendToDevice(token,payload);
+      //For each message check if there was an error
+      const tokensToRemove=[];
+      response.results.forEach((result, index)) => {
+      	const error = result.error;
+      }    
     });
