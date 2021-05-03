aa2b4c147fbfab971a08f00c6d6081a459b49b13
mikeldiokelly <47454996+mikeldiokelly@users.noreply.github.com>
2021-05-03 21:18:16 +0100

    Only journey members can chat. Fixed registration bug by eliminating unused location components.

--- app/src/main/java/com/example/traveltogether/Model/User.java
+++ app/src/main/java/com/example/traveltogether/Model/User.java
@@ -8,22 +8,19 @@ import java.util.List;
 public class User {
 
     public String firstName, lastName, age, email, id, gender;
-    //private String id;
-    Point perm_res, currLoc;
     double avgRating;
     int numRating;
 
     public User() {
     }
 
-    public User(String first_name, String last_name, String age, String email, String id, String gender, com.mapbox.geojson.Point perm_res, com.mapbox.geojson.Point curr_loc) {
+    public User(String first_name, String last_name, String age, String email, String id, String gender) {
         this.firstName = first_name;
         this.lastName = last_name;
         this.age = age;
         this.email = email;
         this.id = id;
         this.gender = gender;
-        this.perm_res = perm_res;
 //        this.currLoc = curr_loc;
 
         this.numRating = 0;
